d5b56c2065f6868354a2fe50ead48f68689710f3
Mar Jayson San Agustin <sanagustinjayson@gmail.com>
2020-06-03 13:53:10 +0800

    refactor: compose todo-item component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <input class="todo-checkbox" v-bind:id="'item_' + todoItem.id" v-model="todoItem.done" type="checkbox">
+        <label v-bind:for="'item_' + todoItem.id"></label>
+        <span class="todo-text">{{ todoItem.title }}</span>
+        <span class="delete" @click="deleteItem()"></span>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ['todoItem'],
+  methods: {
+    deleteItem () {
+      console.log(this.todoItem);
+      this.$emit('todoItemDeleted', this.todoItem);
+    }
+  }
+};
+</script>
